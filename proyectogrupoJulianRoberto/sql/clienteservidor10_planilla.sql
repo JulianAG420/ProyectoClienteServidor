43b5587213dce79cc440d6e87f9bfc1701a1cb88
JulianAG420 <101308341+JulianAG420@users.noreply.github.com>
2022-12-13 00:00:55 -0600

    pruebas socket

---
+++ proyectogrupoJulianRoberto/sql/clienteservidor10_planilla.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: clienteservidor10
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `planilla`
+--
+
+DROP TABLE IF EXISTS `planilla`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `planilla` (
+  `idhistorial` varchar(45) NOT NULL,
+  `cedula` int NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `salarioxhora` float NOT NULL,
+  `mes` int NOT NULL,
+  `anio` int NOT NULL,
+  `horastrabajadas` double NOT NULL,
+  `total` float NOT NULL,
+  PRIMARY KEY (`idhistorial`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `planilla`
+--
+
+LOCK TABLES `planilla` WRITE;
+/*!40000 ALTER TABLE `planilla` DISABLE KEYS */;
+INSERT INTO `planilla` VALUES ('1-1670819696139',4544545,'gghghg',1000,12,2001,2,2000),('1-1670821738693',6565656,'dssdsds',5000,5,2016,5,25000),('2-1670822077190',6565656,'dssdsds',5000,5,2016,8,40000);
+/*!40000 ALTER TABLE `planilla` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-12-12 23:55:45
